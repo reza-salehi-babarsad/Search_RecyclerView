fb949be38e426d7669ad3634b0da48be88c25c89
reza <rezasalehibabarsad@hotmail.com>
2023-06-02 17:31:00 +0330

    final

--- app/src/main/java/com/rezoo/search_recyclerview/MainActivity.kt
+++ app/src/main/java/com/rezoo/search_recyclerview/MainActivity.kt
@@ -3,7 +3,9 @@ package com.rezoo.search_recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rezoo.search_recyclerview.adapter.MyAdapter
 import com.rezoo.search_recyclerview.data.Person
@@ -12,7 +14,7 @@ import com.rezoo.search_recyclerview.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SearchView.OnQueryTextListener {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private val myAdapter: MyAdapter by lazy { MyAdapter() }
@@ -24,9 +26,50 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerview.layoutManager=LinearLayoutManager(this)
         binding.recyclerview.adapter= myAdapter
 
-        val person=Person(1,"ahmad","salari",32)
+        //val person=Person(1,"ahmad","salari",32)
+       // mainViewModel.insertData(person)
+       // val person=Person(2,"ali","karimi",45)
+       // mainViewModel.insertData(person)
+        val person=Person(3,"nader","farhadi",85)
         mainViewModel.insertData(person)
 
         mainViewModel.readData.observe(this,{myAdapter.setData(it)})
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu,menu)
+
+        val search = menu?.findItem(R.id.menu_search)
+        val searchView = search?.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query!=null){
+            searchDatabase(query)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(query: String?): Boolean {
+        if (query!=null){
+            searchDatabase(query)
+        }
+        return true
+
+    }
+
+    fun searchDatabase (query: String?){
+        val searchQuery = "%$query%"
+
+        mainViewModel.searchDatabase(searchQuery).observe(this,{ list ->
+            list.let {
+                myAdapter.setData(it)
+            }
+
+        })
+
     }
 }
--- app/src/main/java/com/rezoo/search_recyclerview/viewModel/MainViewModel.kt
+++ app/src/main/java/com/rezoo/search_recyclerview/viewModel/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.rezoo.search_recyclerview.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.room.Query
 import com.rezoo.search_recyclerview.data.Person
 import com.rezoo.search_recyclerview.data.PersonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,13 +14,17 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: PersonRepository
-): ViewModel(){
-    val readData =repository.readData().asLiveData()
+) : ViewModel() {
+    val readData = repository.readData().asLiveData()
 
-    fun insertData(person: Person){
+    fun insertData(person: Person) {
         viewModelScope.launch {
             repository.insertData(person)
         }
+    }
+
+    fun searchDatabase(searchQuery: String):LiveData<List<Person>>{
+        return repository.searchDatabase(searchQuery).asLiveData()
 
     }
 }
